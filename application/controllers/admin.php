7e058b14b0393fed84b5de4beadfccbaa88bb862
Muhammad Rizki <muhammadrizkitkj123@gmail.com>
2023-01-21 08:44:35 +0800

    repair dashboard function

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,9 +1,12 @@
 <?php 
  
-class Admin extends CI_Controller{
+class Admin extends CI_Controller
+{
  
 	function __construct(){
 		parent::__construct();
+		$this->load->model('barang_model');
+        $this->load->model('peminjaman_model');
 	
 		if($this->session->userdata('status') != "login"){
 			return redirect ("login_controller/masuk"); //jika tidak login dan diback dari browser akan tetap pada vi_login
@@ -11,6 +14,30 @@ class Admin extends CI_Controller{
 	}
  
 	function index(){
-		$this->load->view('home');
-	}
+		// $barangs = $this->barang_model->select();
+		// 	$data_produk['barangs'] = null;
+		// 	foreach ($barangs as $prd) {
+		// 		$data_produk['barangs'][]=$prd;
+
+        $key['status'] = 'y';
+        $peminjaman = $this->peminjaman_model->select($key);
+        if (count($peminjaman) == 0) {
+            unset($peminjaman);
+            $peminjaman['tanggal'] = date('Y-m-d');
+            $peminjaman['status'] = 'y';
+            $this->peminjaman_model->insert($peminjaman);
+        }
+        $peminjaman = $this->peminjaman_model->select($key)[0];
+        $key_detail['id_peminjaman'] = $peminjaman['id_peminjaman'];
+        $data['detilss'] = $this->peminjaman_model->select_detail($key_detail);
+        $data['barangs'] = $this->barang_model->select();
+        $data['peminjaman'] = $peminjaman;
+
+        // $konten['konten']=$this->load->view('home',$data,true);
+        // $this->load->view('template',$konten);
+        $this->load->view('home', $data);
+    }
+	
+
+   
 }
